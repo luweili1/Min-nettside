ae8963ec6943ae877923ef3bcd5fa6fc959c0e21
Luwei.Li <luweili1@hotmail.com>
2025-03-30 15:41:56 +0200

    mer om meg

--- app/om-meg/page.tsx
+++ app/om-meg/page.tsx
@@ -1,3 +1,14 @@
 export default function OmMegPages() {
-    return <h1 className="text-center p-10"> Om meg</h1>;
+  return (
+    <div className="text-center p-10">
+      <h1>Om meg</h1>
+      <p className="mt-4">
+        Hei! Mitt navn er Luwei, og jeg er utdannet lektor i biologi, kjemi,
+        naturfag og matematikk. Jeg er for øyeblikk student ved Universitet i
+        Bergen og tar en bachelor i datasikkerhet. På fritiden liker jeg å sy,
+        strikke, lese og henge med venner.
+      </p>
+      <p className="mt-2">Se gjerne på mine prosjekter!</p>
+    </div>
+  );
 }
